28ebc71a5b1622d19f39ab96b287c93b5dd0bbf1
Sachin-Sati <satisachin619@gmail.com>
2025-04-16 20:51:26 +0530

    fileURLToPath added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 import productRoutes from "./routes/product.route.js";
 
