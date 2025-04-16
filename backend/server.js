c966d5b0335295ce505219ac26de6108c74a3134
Sachin-Sati <satisachin619@gmail.com>
2025-04-16 21:15:46 +0530

    mongoDB connection update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 import productRoutes from "./routes/product.route.js";
 
@@ -15,26 +14,20 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 app.use(express.json());    // allows us to accept json data in req.body
 
 app.use("/api/products", productRoutes);
 
 // if env -> production, set dist folder as static folder
-if (process.env.NODE_ENV === 'production') {
-    const frontendBuildPath = path.join(__dirname, '../frontend/dist');
-    app.use(express.static(frontendBuildPath));
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(frontendBuildPath, 'index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:"+PORT);
 });
-
-// JKtw64nl4B1W2I4R - MongoDB Cluster Password
