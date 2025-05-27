f5e70d56f41590cb7b25ae55183a39a23109a07d
Sachin-Sati <satisachin619@gmail.com>
2025-05-27 15:18:19 +0530

    /backend new origin added

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // CORS - Express app allows requests from frontend
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://product-store-frontend-jade.vercel.app'],
+  origin: ['http://localhost:5173', 'https://product-store-frontend-jade.vercel.app', 'https://product-store-frontend-cch3902fx-sachin-satis-projects.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
